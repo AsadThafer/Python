07e819a8dd4c42a14c5b345639b53e3dc5522003
Asad Asad <43581992+AsadThafer@users.noreply.github.com>
2022-06-05 08:45:15 +0300

    Add files via upload

---
+++ AI Palestine AstarGBFS fullmap draw latestversion/editresolutioncalc.py
@@ -0,0 +1,5 @@
+#f = open("cities.txt")
+#for i in f.readlines():
+#    node_city_val = i.split()
+#    h,x,y=[str(node_city_val[0]),int(node_city_val[1]), int(node_city_val[2])]
+#    print(h,x+100,y)
